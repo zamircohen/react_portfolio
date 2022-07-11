9c6d37d3b09d195983eab9448664b247c9f8490b
zamircohen <zamir_cohen@hotmail.com>
2022-07-11 21:46:59 +0200

    Added images and worked on timeline function

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,22 +9,20 @@ import { Chrono } from "react-chrono";
 
 const items = [
   {
-  title: "May 1940",
-  cardTitle: "Dunkirk",
-  cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
-  },
-  {
-    title: "June 1977",
-    cardTitle: "Birth of Zamir",
-    cardSubtitle:"I was born on April 22nd 1977",
+    title: "2022",
+    cardTitle: "Backend Development",
     cardDetailedText: "It was a long time ago, but I was born on April 22nd 1977.",
   },
   {
     title: "May 1940",
     cardTitle: "Summer of War",
-    cardSubtitle:"Men of the British Expeditionary Force (BEF) wade out to..",
-    cardDetailedText: "Men of the British Expeditionary Force (BEF) wade out to..",
+    cardDetailedText: "Lorem Men of the British Expeditionary Force (BEF) wade out to. Lorem Men of the British Expeditionary Force (BEF) wade out to. Lorem Men of the British Expeditionary Force (BEF) wade out to.Lorem Men of the British Expeditionary Force (BEF) wade out to.",
     },
+  {
+    title: "June 1977",
+    cardTitle: "Birth of Zamir",
+    cardDetailedText: "It was a long time ago, but I was born on April 22nd 1977.",
+  },
 ];
 
 
@@ -35,32 +33,37 @@ export default function About() {
     <Wrapper id="about">
         <Block>    
             <BlockTitle main>About me</BlockTitle>     
-            <BlockText>I´m a Social and curious team player with a huge interest in new experiences and personal development. I have a versatile background with experience in areas such as the technology, service- and travel industry. <br /><br />
-             I´m in the process of expanding my skills and currently studying Full Stack Web Development at Nackademin, Stockholm, which is a 2-year higher vocational education.<br /><br />
-             I´m looking forward to a career in web development. Please contact me <HashLink smooth to="/#contact" style={{ color: 'white' }} activeStyle={{ color: 'blue' }}>here</HashLink> for further inquieries.
+            <BlockText>I am a Social and curious team player with a huge interest in new experiences and personal development. I have a versatile background with experience in areas such as the technology, service- and travel industry. <br /><br />
+             I am in the process of expanding my skills and currently studying Full Stack Web Development at Nackademin, Stockholm, which is a 2-year higher vocational education.<br /><br />
+             I am looking forward to a career in web development. Please contact me <HashLink smooth to="/#contact" style={{ color: 'white' }} activeStyle={{ color: 'blue' }}>here</HashLink> for further inquieries.
             </BlockText>
         </Block>
 
         <Block>
-            <BlockTitle main>Education</BlockTitle>
+            <BlockTitle main>My CV</BlockTitle>
             <BlockText>
             <div style={{ width: "100%", height: "600px", hideControls: "true", }}>
               <Chrono 
                 items={items} 
+                mode="VERTICAL"
                 hideControls={true} 
-                slideShow={false}
-                onRestartSlideshow={true}
-                slideShowRunning={true}
                 scrollable={{ scrollbar: true }}
                 theme={{
-                  primary: 'lightblue',
+                  primary: 'white',
                   secondary: 'transparent',
-                  secondaryText: 'white',
                   cardBgColor: 'white',
                   cardForeColor: 'black',
+                  cardTitleColor: 'black',
                   titleColor: 'black',
+                  titleColorActive: 'white',
                 }}
-              />
+                fontSizes={{
+                  cardText: '2rem',
+                  cardTitle: '3rem',
+                  cardDetailedText: '1.5rem',
+                }}
+              >
+              </Chrono>
             </div>
             </BlockText>
         </Block>
