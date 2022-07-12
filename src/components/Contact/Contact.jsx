18d2e4831bdf37a5764363fd1504196e3d38bb4a
zamircohen <zamir_cohen@hotmail.com>
2022-07-12 22:00:59 +0200

    Contact form sends email to me

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
-import { SectionTitle, Block, Section, SectionText, Wrapper, BlockTitle} from '../Header/HeaderStyles';
-import { Form } from './ContactStyles';
-
+import React, { useRef } from 'react'
+import { Block, Wrapper, BlockTitle} from '../Header/HeaderStyles';
+// import { Form } from './ContactStyles';
+// import { useState } from 'react';
+// import { send } from 'emailjs'
+import emailjs from 'emailjs-com';
+import './FormStyles.css';
 
 export default function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "service_y2qtqn8", 
+        "template_ng327zx", 
+        form.current, 
+        "X6YypPlAQ9WEwC-AE"
+        ).then(
+          result => console.log(result.text),
+          error => console.log(error.text)
+        );
+    };
+
+
   return (
     <Wrapper id="contact">
         <Block>    
             <BlockTitle main>Contact me.</BlockTitle>     
-            <Form>
-                
-                <input type="text" placeholder='Your name*' required="true"/>
-                
-                <input type="text" placeholder='Your comnpany'/>
-                
-                <input type="text" placeholder='Your email*' />
-                
-                <input type="tel" placeholder='Phonenumber' />
-                
-                <textarea placeholder='Your message'></textarea>
+
+            <form className="form" ref={form} onSubmit={sendEmail}>
+                <input className="input" type="text" placeholder='Your name*' name="name" required="true"/>
+                <input className="input" type="text" placeholder='Your comnpany' name="company" />
+                <input className="input" type="text" placeholder='Your email*' name="email" />
+                <input className="input" type="tel" placeholder='Phonenumber' name="phone" />
+                <textarea className="textarea" placeholder='Your message' name="message" ></textarea>
                 <br />
-                <button> SUBMIT </button>
-            </Form>
+                <button className="button" type="submit"> SUBMIT </button>
+              </form>
+
         </Block>
     </Wrapper>
   )
