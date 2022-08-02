3bbf73665bfcccde444cd17a6157ce8422a2eef5
zamircohen <zamir_cohen@hotmail.com>
2022-08-02 18:13:57 +0200

    Added fade function to About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { Block, BlockTitle, BlockText, Wrapper } from '../Header/HeaderStyles';
 import { HashLink } from 'react-router-hash-link';
 import { Fade } from "react-awesome-reveal";
-import { AiFillFacebook, AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+// import { AiFillFacebook, AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import { Chrono } from "react-chrono";
 
 
@@ -30,14 +30,21 @@ const items = [
 export default function About() {
   return (
     <Wrapper id="about">
+      
         <Block>    
+        <Fade delay={300}>
             <BlockTitle main>About me</BlockTitle>     
             <BlockText>I am a Social and curious team player with a huge interest in new experiences and personal development. I have a versatile background with experience in areas such as the technology, service- and travel industry. <br /><br />
              I am in the process of expanding my skills and currently studying Full Stack Web Development at Nackademin, Stockholm, which is a 2-year higher vocational education.<br /><br />
              I am looking forward to a career in web development. Please contact me <HashLink smooth to="/#contact" style={{ color: 'white' }} activeStyle={{ color: 'blue' }}>here</HashLink> for further inquieries.
             </BlockText>
+            </Fade>
         </Block>
+        
+
+        
         <Block>
+        <Fade delay={300}>
             <BlockTitle main>My CV</BlockTitle>
             {/* <BlockText> */}
             <div style={{ width: "100%", height: "600px", hideControls: "true", }}>
@@ -68,7 +75,9 @@ export default function About() {
 
             </div>
             {/* </BlockText> */}
+            </Fade>
         </Block>
+        
 
     </Wrapper>
   )
